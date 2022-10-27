8d042d79c66ea87750c506212fcca245f180b0bc
Nicolas Wyss <nicolasfwyss@outlook.com>
2022-10-27 21:48:07 +0200

    initial commit

---
+++ app/Http/Controllers/StartpageController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class StartpageController extends Controller
+{
+    //
+
+    public function index()
+    {
+        // Letze 5 Beträge, neuste oben, gelöschte ausgeblendet
+        //$posts = Post::all()->sortByDesc('created_at'); =
+        $posts = Post::with('user')
+            ->latest()
+            ->active()
+            ->limit(5)
+            ->get();
+
+
+
+        return view('index', compact('posts'));
+    }
+
+
+}
