1575539686132959c809cc94f0706618ad203fb4
Nicolas Wyss <nicolasfwyss@outlook.com>
2022-10-28 14:02:22 +0200

    Testing Setup

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Post;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
@@ -14,8 +15,34 @@ class ExampleTest extends TestCase
      */
     public function test_example()
     {
-        $response = $this->get('/');
+        $response = $this->get('/welcome');
 
         $response->assertStatus(200);
+
+
+        $post = Post::withTrashed()->get();
+        $this->assertCount(100, $post);
     }
+
+    /**
+     * @test
+     *
+     * Valid test because '@test' is in JavaDoc Comment
+     */
+    public function nameForTest()
+    {
+        $this->assertTrue(true);
+    }
+
+
+    /**
+     * Valid test because function name starts with test
+     */
+    public function testName()
+    {
+        $this->assertTrue(true);
+    }
+
+
+
 }
